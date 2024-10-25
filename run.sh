27260b9edc092b78410f5d3efdb24bc887d98d13
Ville-Matti Kaartinen (EXT) <ext-ville-matti.kaartinen@elisa.fi>
2024-10-25 13:39:16 +0300

    run.sh with clean

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+set -ex
+
+cleanPrices() {
+   rm -f current.json prices.json
+}
+
 rm -rf build/*
-#rm -f current.json prices.json
 npx tsc
+if [ "$1" == "clean" ]; then
+   cleanPrices
+fi
 node build/spot.js
